349c80b468d38a3cc4ef95e3895fd4ef243f6d34
onteriowright <onteriowright@yahoo.com>
2020-03-06 13:17:06 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nssStudents = new List<Student>()
+            {
+                new Student()
+                {
+                Name = "Spencer",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Heidi",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Namita",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Holden",
+                CohortName = "Day Cohort 37"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Aryn",
+                CohortName = "Day Cohort 35"
+                },
+
+                new Student()
+                {
+                Name = "Jansen",
+                CohortName = "Day Cohort 37"
+                }
+
+            };
+
+            var names = nssStudents.Select(student => student.Name);
+
+            var studentDescription = nssStudents.Select(student =>
+            {
+                return $"{student.Name} is a student in {student.CohortName}";
+            });
+
+            var StudentsIn37 = nssStudents.Where(student => student.CohortName == "Day Cohort 37");
+        }
+    }
+}
